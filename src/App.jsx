fd9105f6146be61ac47a6a5de3ef6be2862c447d
Manuel Hurtado <manuhurtado_24@hotmail.com>
2025-04-14 12:14:49 -0600

    ADD: Scroll to Top

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
 import ContactoPage from './pages/ContactoPage'
 import { routes } from './utils/rutas'
+import { useEffect } from 'react'
 
 function App() {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location.pathname])
+
   return (
     <>
       <Routes>
