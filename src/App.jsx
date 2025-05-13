302280409106cf60162fdc522f422561a5e5e898
Alex <alexrobertomata@hotmail.com>
2025-05-12 18:00:22 -0600

    ADD: fuente Dosis

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes, useLocation } from 'react-router-dom'
-import './App.css'
 import HomePage from './pages/HomePage'
 import ContactoPage from './pages/ContactoPage'
 import { routes } from './utils/rutas'
 import { useEffect } from 'react'
+import './App.css'
+import './assets/css/fonts.css'
+import './assets/css/styles.css'
 
 function App() {
   const location = useLocation()
